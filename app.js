8589cb9d538a20c32b174a761ed8c5910a48c699
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-04-23 17:41:30 +0200

    more handlebars, design, listanaslovi

--- app.js
+++ app.js
@@ -14,11 +14,55 @@ app.engine(
     extname: 'hbs'
   })
 );
+
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+
+var notesproto0 = [
+{naslov: "aaaaaaaaaaaaaaaaaaab"},
+{naslov: "Peter"},
+{naslov:"CEne"},
+{naslov: "aaaaaaaaaaaaaaaaaaab"},
+{naslov: "Peter"},
+{naslov:"CEne"},
+{naslov: "aaaaaaaaaaaaaaaaaaab"},
+{naslov: "Peter"},
+{naslov:"CEne"},
+{naslov: "aaaaaaaaaaaaaaaaaaab"},
+{naslov: "Peter"},
+{naslov:"CEne"},
+{naslov: "aaaaaaaaaaaaaaaaaaab"}
+
+
+
+]
+
+
+//glavna stran
 app.get('/', (req, res) => {
-	res.render('home', {id0: 'test'});
+	console.log(notesproto0[0].naslov)
+
+	//skrajša dolžino naslovov za listo
+
+	var listanaslovov = new Array;
+
+	for (let index = 0; index < notesproto0.length; index++) {
+		var trenutennaslov = (notesproto0[index].naslov).substring(0,20)
+
+		if (((notesproto0[index].naslov).length) > 20) {
+			trenutennaslov = trenutennaslov + "..."
+		}
+
+		listanaslovov.push(trenutennaslov)
+		
+		console.log(listanaslovov)
+		
+	}
+
+
+
+	res.render('home', {id0: 'test', listanaslovov: listanaslovov});
 });
 
 
