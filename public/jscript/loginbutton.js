35102934bb95f76588d6d46bc6ffb40b991f8dac
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-04-24 23:00:27 +0200

    večinoma dokončan frontend

--- public/jscript/loginbutton.js
+++ public/jscript/loginbutton.js
@@ -1,5 +1,5 @@
 var button = document.getElementById('id_button')
-
+var shrani =  document.getElementById('shrani')
 
 
 button.onmouseover = () => {
@@ -8,5 +8,14 @@ button.onmouseover = () => {
 
 button.onmouseleave = () => {
 	button.style.backgroundColor = "#b0bec5"
+}
+
+
+shrani.onmouseover = () => {
+	shrani.style.backgroundColor = "#e2f1f8";
+}
+
+shrani.onmouseleave = () => {
+	shrani.style.backgroundColor = "#808e95"
 }
 
